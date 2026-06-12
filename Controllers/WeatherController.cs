33a1b75041a91b274cfc7384938ce9819f260822
Ashvink95 <ashvin@nevtan.com>
2026-06-12 23:39:15 +0530

    Add Controllers/WeatherController.cs

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
+    };
+
+    [HttpGet(Name = "GetWeather")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
